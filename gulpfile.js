d9b77d90588da123d5264d84545e785ee63206dc
Mahmut Duva <mahmut.duva@freebirdairlines.com>
2016-05-16 22:43:46 +0300

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 var gulp = require('gulp');
+var minify = require('gulp-minify');
+var open = require('gulp-open');
+
 
 gulp.task('default', function() {
-    // place code for your default task here
+    gulp.src('src/puk-start.js')
+        .pipe(minify({
+            ext:{
+                min:'.min.js'
+            },
+            exclude: ['tasks'],
+            ignoreFiles: ['.min.js']
+        }))
+        .pipe(gulp.dest('src'));
+
+    gulp.src('example/index.html')
+        .pipe(open());
 });
+
